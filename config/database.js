791a7da323bf57cdef5ca51b57a126e596ab4dfd
Lakshmipriya <slpriya.as@gmail.com>
2020-06-18 11:48:02 -0700

    Folder Structure Changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const conStr = require('./keys').mongoURI;
 
-const db = mongoose.connect(conStr,{ useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(conStr,{ useNewUrlParser: true , useUnifiedTopology: true})
         .then( () => {console.log("Successfully connected to the database");} )
         .catch((err) => {
   console.log('Could not connect to the database. Exiting now...', err);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,77 +1,20 @@
 const router = require('express').Router();
-const User = require('../models/User.model');
-const gravatar = require('gravatar');//import gravatar
-const bcrypt = require('bcryptjs');
-
-router.post('/register', (req, res) => {
-   User.findOne({ email: req.body.email })
-    .then((user) => {
-      console.log("Chk DB");
-      processRequest(user, req, res)
-    })
-    .catch((err) => {
-      console.log("err chk");
-      res.status(500).json({
-        message: "Some internal error Occured " + err.message
-      })
-    });
-
-});
+const controller = require('../controllers/user.controller');
 
 
-function processRequest(user, req, res) {
-  console.log("processRequest");
-  if (user) {
-    console.log("processRequest1");
-    return res.status(400)
-      .json({ Success: false, Message: "Registered Email already exists" });
-  }
+// @route   POST  http://localhost:7500/api/users/register
+// @desc    Register User
+// @access  public
+router.post('/register' , controller.register);
 
-  createUser(req, res);
-
-}
-
-function createUser(req, res) {
-
-  const avatar = gravatar.url(req.body.email,
-     {
-        s: '100', r: 'pg', d: 'mm', protocol: 'http'
-     });  
-  const newUser = new User({
-      name : req.body.name ,
-      username: req.body.username ,
-      email : req.body.email,
-      password : req.body.password ,
-      mobile : req.body.mobile,
-      avatar
-  });
-   saveUser(newUser , res);
-}
-
-function saveUser(newUser , res){  
-  bcrypt.genSalt(10 , (err , salt) => {
-    if(err) throw err;
-
-    bcrypt.hash(newUser.password , salt , (err , hash) => {
-      if(err) throw err;
-
-      newUser.password = hash;
-      console.log(newUser);
-      newUser.save()
-            .then( (user) => {
-              res.json({ success : true , user : user , message : 'User Successfullu Registered!'});
-            })
-            .catch(err => {
-              res.status(500).send({
-                message: err.message || "Some error occurred while creating the User."
-              });
-            });
-    });
-
-  });
+// @route   POST http://localhost:7500/api/users/login
+// @desc    Login User
+// @access  public
 
 
-}
+router.post('/login' , controller.login); 
+
+
 
 
 module.exports = router;

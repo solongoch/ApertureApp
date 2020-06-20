08182727dde58c490aea5b112225d3a18587ef3f
Lakshmipriya <slpriya.as@gmail.com>
2020-06-19 20:07:31 -0700

    changes in server.js comments added and database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const conStr = require('./keys').mongoURI;
 
-mongoose.connect(conStr,{ useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true})
-        .then( () => {console.log("Successfully connected to the database");} )
-        .catch((err) => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
-
-
+const db = mongoose.connect(conStr, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => { console.log("Successfully connected to the database"); })
+  .catch((err) => {
+    console.log('Could not connect to the database. Exiting now...', err.message);
+    process.exit();
+  });
+ 
+ module.exports =  db;
+  
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+// Create the Express application
 const app = express();
+
 const users = require('./routes/api/user');
 const bodyparser = require('body-parser');
 const port = require('./config/keys').port;
 const passport = require('passport');
 
-require('./config/database');
+// Configures the database and opens a global connection
+const db = require('./config/database');
+
+// Configure body parser
 app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
 
+//This will initialize the passport object on every request
 app.use(passport.initialize());
+
 require('./config/passport')(passport);
+
 // Homepage route
 app.get('/', (req, res) => res.send('Aperture'));
 
+// Server listens on http://localhost:7500
 app.listen(port, () => console.log(`Server has started on http://localhost:${port}`));
 
+// Imports all of the routes
 app.use('/api/users' , users);

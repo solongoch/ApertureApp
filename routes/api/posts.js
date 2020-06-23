789b346f6d187d1e6dee393f60ec1742f98a2701
Lakshmipriya <slpriya.as@gmail.com>
2020-06-23 11:46:55 -0700

    Post Schema and Profile Schema has been created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const Posts = require('../../models/Posts');
+const User = require('../../models/User');
 
 router.get('/test', (req,res) => res.json({msg: 'Posts works!'}))
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 router.get('/test', (req,res) => res.json({msg: 'Profile works!'}))
 

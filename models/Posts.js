3a7baa0c785df8e1e4f4dd89230598f173c9f8f9
Solongo <soogii1005@gmail.com>
2020-06-26 01:13:18 -0700

    edited Post model, added get '/', get ':id', delete ':id' logic in posts

--- models/Posts.js
+++ models/Posts.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   postedBy: { type: Schema.Types.ObjectId, ref: "users" },
   photo: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, required: false },
   likes: [{ type: Schema.Types.ObjectId, ref: "users" }],
   comments: [
     {
       text: String,
-      postedBy: { type: Schema.Types.ObjectId, ref: "users" }
+      commentedBy: { type: Schema.Types.ObjectId, ref: "users" }
     }
   ],
   timePosted: { type: Date, default: Date.now }
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const Posts = require('../../models/Posts');
+const Post = require('../../models/Posts');
 const User = require('../../models/User');
 
 router.get('/test', (req,res) => res.json({msg: 'Posts works!'}))
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => res.json(post))
+    .catch((err) =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch((err) =>
+          res.status(404).json({ postnotfound: "No post found" })
+        );
+    });
+  }
+);
 
 module.exports = router;

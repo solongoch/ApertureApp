70ca0c9ed2e38283fe51138b75ca5976566481d6
Solongo <soogii1005@gmail.com>
2020-06-26 00:59:02 -0700

    change in Posts model

--- models/Posts.js
+++ models/Posts.js
@@ -1,26 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-
-  title: { type: String, required: true },
-  body: { type: String, required: true },
+  postedBy: { type: Schema.Types.ObjectId, ref: "users" },
   photo: { type: String, required: true },
-  likes: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+  description: { type: String, required: true },
+  likes: [{ type: Schema.Types.ObjectId, ref: "users" }],
   comments: [
     {
       text: String,
-      postedBy: { type: Schema.Types.ObjectId, ref: 'users' }
+      postedBy: { type: Schema.Types.ObjectId, ref: "users" }
     }
   ],
-  postedBy: { type: Schema.Types.ObjectId, ref: 'users' },
-  timePosted: { type: Date, default: Date.now } 
+  timePosted: { type: Date, default: Date.now }
 });
 
-
-
-module.exports = posts = mongoose.model('posts', PostSchema);
-
-
-
-
+module.exports = posts = mongoose.model("posts", PostSchema);
--- validation/login.js
+++ validation/login.js
@@ -4,14 +4,15 @@ const isEmpty = require("./is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 
-  //Loginid length Validation
-  if (!validator.isLength(data.loginId, { min: 3, max: 30 })) {
-    errors.loginId = 'Check the length of the Login id';
-  }
- //Login Id IsEmpty Check
-  if (isEmpty(data.loginId)) {
-    errors.loginid = 'Login id should not be empty';
-  }
+//   // Loginid length Validation
+//   if (!validator.isLength(data.loginId, { min: 3, max: 30 })) {
+//     errors.loginId = 'Check the length of the Login id';
+//   }
+//  //Login Id IsEmpty Check
+//   if (isEmpty(data.loginId)) {
+//     errors.loginid = 'Login id should not be empty';
+//   }
+
   //Password validaiton
   if (!validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must have at least 6 characters";

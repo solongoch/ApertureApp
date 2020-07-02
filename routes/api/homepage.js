9a7bc2b1c385d78b4b3d20eff7aad87e4c0ca2c3
Solongo <soogii1005@gmail.com>
2020-07-02 10:24:22 -0700

    added return statements

--- routes/api/homepage.js
+++ routes/api/homepage.js
@@ -6,16 +6,21 @@ const accessRouteWithOrWithoutToken = require("../../controller/accessRouteWithW
 // @desc    Landing page
 // @access  Public and Private
 router.get("", accessRouteWithOrWithoutToken, (req, res) => {
+  // if user not logged in
   if (!req.isAuthenticated()) {
+    // display logIn/SignUp page
     return res.json({ msg: "Do you want to log in or sign up?" });
   } else {
     const following = req.user.following;
+
+    // if user didn't follow anyone
     if (following.length === 0) {
+      // suggest someone to follow
       return res.json({ msg: "Accounts suggestion to follow" });
     }
 
-    Post.find({ postedBy: { $in: following.map(following => following.user) } })
-      .sort({ timePosted: -1 })
+    Post.find({ postedBy: { $in: following.map(following => following.user) } }) // check postedBy user is in following
+      .sort({ timePosted: -1 }) // sort post date by descending order
       .limit(10)
       .then(posts => {
         return res.send(posts);

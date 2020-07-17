9b835055f2972bf3a87acc5559c8ec7a444c825e
Lakshmipriya <slpriya.as@gmail.com>
2020-07-17 10:22:45 -0700

    Added Unfollow componenet and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Signup from './components/Auth/Signup';
 import ChangePassword from './components/Auth/ChangePassword'
 import followers from './components/Layout/Followers';
 import followings from './components/Layout/Followings';
+import unfollow from './components/Layout/Unfollow';
 
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
           <Route exact path='/changePassword' component={ChangePassword} />
           <Route exact path='/followers' component={followers} />
           <Route exact path='/followings' component={followings} />
+          <Route exact path='/unfollow' component={unfollow} />
         </div>
       </Router>
     );
---
+++ client/src/components/Layout/Unfollow.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logoImage from "../../image/avatar.png";
+import '../css/Unfollow.css';
+
+class Unfollow extends Component {
+  render() {
+    return (
+      <div className="chgpwd-div">
+        <div className="container">
+          <div className="row align-items-center">
+          <div className="mx-auto ">
+            <div className="card">
+              <form>
+                <div className="form-group">
+                  <div className=" col-sm-10 col-md-12 col-lg-12">
+                    <img className="avatar-img " src={logoImage} alt="Avatar" />
+                  </div>
+                  <div className="username-div col-sm-12 col-md-12 col-lg-12">
+                          <p className="username-p">If you change your mind, you'll have to request to follow @keerthikadambala again.
+                          </p>
+                  </div>
+                  <div className="unfollow-div">
+                    <button className=" btn-unfollow btn-common btn-block">Unfollow</button>
+                    <button className="btn-cancel btn-common btn-block">Cancel</button>
+                  </div>
+                  </div>
+               </form>
+            </div>
+          </div> 
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Unfollow;

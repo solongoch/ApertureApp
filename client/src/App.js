def18a65dfa4b1bdf602d57b10166bfe3214d7c6
Lakshmipriya <slpriya.as@gmail.com>
2020-07-17 02:29:34 -0700

    Added Followers and Following and change password component,  css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './components/Auth/Signup';
 import ChangePassword from './components/Auth/ChangePassword'
+import followers from './components/Layout/Followers';
+import followings from './components/Layout/Followings';
+
 
 class App extends Component {
 
@@ -12,6 +15,8 @@ class App extends Component {
         <div className="App container">
           <Route exact path='/' component={Signup} />
           <Route exact path='/changePassword' component={ChangePassword} />
+          <Route exact path='/followers' component={followers} />
+          <Route exact path='/followings' component={followings} />
         </div>
       </Router>
     );

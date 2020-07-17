888b88a887abc7970fbe111740d057eb3d4b886a
Lakshmipriya <slpriya.as@gmail.com>
2020-07-17 16:07:13 -0700

    Followers and Following Allignment Issues fixed

--- client/src/components/Layout/Followers.js
+++ client/src/components/Layout/Followers.js
@@ -9,12 +9,11 @@ class Followers extends Component {
   //   super();    
   // }
   
-
   render() {
     return (
-        <div className='second'>
-          <div className='third'>
-            <div className='followers-container'>
+        <div className='main-wrapper'>
+          <div className='sub-wrapper-div'>
+             <div className='user-container'>
               <h5>Followers</h5>
               <span>
                 <Link to='/profile' className='close'>
@@ -23,37 +22,35 @@ class Followers extends Component {
               </span>
               <hr />
 
-              <div className='container scrolling'>
+              <div className='container scrolluser'>
                    {
                      users.map( (user) => {
                        return(
                         <div className='row' key={user.username}>
                           
-                          <div className='spaceMargin col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
+                          <div className='avatar-div col-2 col-sm-2 col-md-2 col-lg-2 col-xxs-2'>
                             <Link to='#'>
                               <img
-                                className='followers-avatar-icon'
+                                className='user-avatar'
                                 src={user.avatar}
                                 alt='Avatar'
                               />
                             </Link>
                           </div>
-                          <div className='col-sm-6 col-md-6 col-lg-6 col-xxs-6'>
-                              <Link
-                                to='#'
-                                className='fontStyleSizeColor'
-                                style={{ marginLeft: "-50px",textDecoration: 'inherit',
-                                "cursor": "pointer" }}
-                              >
-                               <span className="username"> {user.username} </span>
-                              </Link>
-                              <div style={{ marginTop: "-4px", marginLeft: "-50px" }}>
-                              <span className="name"> {user.name}  </span>
-                              </div>
+                          <div className='userinfo-div col-7 col-sm-7 col-md-7 col-lg-7 col-xxs-7'>
+                                  <Link
+                                  to='#' 
+                                  className='username-link'
+                                  style={{ marginLeft: "-60px",textDecoration: 'inherit',
+                                  "cursor": "pointer" }}                                 
+                                >
+                                <span className="username"> {user.username} </span>
+                                </Link>
+                                <span className="name"> {user.name}  </span>
                           </div>
-                          <div className='spaceMargin col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
+                          <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
                               <button
-                                className='btn btn-primary-outline btn-lg'
+                                className='btn btn-primary-outline btn-lg float-right'
                                 style={{
                                   lineHeight: "2px",
                                   float: "right",
--- client/src/components/Layout/Followings.js
+++ client/src/components/Layout/Followings.js
@@ -3,80 +3,72 @@ import "../css/Follow.css";
 import { Link } from "react-router-dom";
 import users from './Data';
 
-
 class Followings extends Component {
+
   // constructor() {
   //   super();    
   // }
   
   render() {
     return (
-      <div className='first'>
-        <div className='second'>
-          <div className='third'>
-            <div className='followers-container'>
-              <h5>Followings </h5>
+        <div className='main-wrapper'>
+          <div className='sub-wrapper-div'>
+             <div className='user-container'>
+              <h5>Followings</h5>
               <span>
                 <Link to='/profile' className='close'>
-                    <i className="fa fa-times" aria-hidden="true"></i>
+                <i className="fa fa-times" aria-hidden="true"></i>
                 </Link>
               </span>
               <hr />
 
-              <div className='container scrolling'>
-              {
-                users.map( (user) => {
-                  return(
-                    <div className='row' key={user.username}>
-                      <div className='spaceMargin col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
-                        <Link to='#'>
-                          <img
-                            className='followers-avatar-icon'
-                            src={user.avatar}
-                            alt='Avatar'
-                          />
-                        </Link>
-                      </div>
-
-                      <div className='col-sm-6 col-md-6 col-lg-6 col-xxs-6'>
-                        <Link
-                          to='#'
-                          className='fontStyleSizeColor'
-                          style={{ marginLeft: "-50px" , textDecoration: 'inherit',
-                                      "cursor": "pointer"}}
-                        >
-                          <span className="username"> {user.username} </span>
-                        </Link>
-                        <div style={{ marginTop: "-4px", marginLeft: "-50px" }}>
-                        <span className="name"> {user.name}  </span>
-                        </div>
-                      </div>
-
-                      <div className='spaceMargin col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
-                        <button
-                          className='btn btn-primary-outline btn-lg'
-                          style={{                            
-                            lineHeight: "2px",
-                            float: "right",
-                            height: "30px",
-                          }}
-                          // onClick={this.handleClick}
-                        >
-                        
-                        Following
-                          {/* {this.state.isToggleOn ? "Follow" : "Following"} */}
-                        </button>
-                      </div>                    
-                    </div>
-                  )
-                })
-            }  
+              <div className='container scrolluser'>
+                   {
+                     users.map( (user) => {
+                       return(
+                        <div className='row' key={user.username}>
+                          
+                          <div className='avatar-div col-2 col-sm-2 col-md-2 col-lg-2 col-xxs-2'>
+                            <Link to='#'>
+                              <img
+                                className='user-avatar'
+                                src={user.avatar}
+                                alt='Avatar'
+                              />
+                            </Link>
+                          </div>
+                          <div className='userinfo-div col-7 col-sm-7 col-md-7 col-lg-7 col-xxs-7'>
+                                  <Link
+                                  to='#' 
+                                  className='username-link'
+                                  style={{ marginLeft: "-60px",textDecoration: 'inherit',
+                                  "cursor": "pointer" }}                                 
+                                >
+                                <span className="username"> {user.username} </span>
+                                </Link>
+                                <span className="name"> {user.name}  </span>
+                          </div>
+                          <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xxs-3'>
+                              <button
+                                className='btn btn-primary-outline btn-lg float-right'
+                                style={{
+                                  lineHeight: "2px",
+                                  float: "right",
+                                  height: "30px",
+                                }}// onClick={this.handleClick}
+                               >      Followings  {/* {this.state.isToggleOn ? "Follow" : "Following"} */}
+                              </button>
+                          </div>
+                         </div>                          
+                        )
+                      })
+                  }  
+                                           
               </div>
-              {/* followers-container ends */}
+       
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }

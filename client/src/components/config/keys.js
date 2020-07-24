84a5bdc2c0b4f73fccbfdddd482c167575806997
Solongo <soogii1005@gmail.com>
2020-07-24 14:52:14 -0700

    fixed Keys.js file

--- client/src/components/config/keys.js
+++ client/src/components/config/keys.js
@@ -1,7 +1,5 @@
-const cloudinary ={
+module.exports = {
   UPLOAD_PRESET: 'Aperture',
   CLOUD_NAME:'domxqhrc0',
   URL: 'https://api.cloudinary.com/v1_1/domxqhrc0/image/upload'
 }
-
-export default cloudinary;

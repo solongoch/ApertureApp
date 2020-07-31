ad1d1103d0e5c23db36431215cca46ecefa82fce
Lakshmipriya <slpriya.as@gmail.com>
2020-07-31 04:33:43 -0700

    changed class=className

--- client/src/components/Profile/ProfilePosts.js
+++ client/src/components/Profile/ProfilePosts.js
@@ -8,19 +8,19 @@ class ProfilePosts extends Component {
     const { posts } = this.props;
     return (
 
-      <div class="profileposts">
+      <div className="profileposts">
         {posts.map(post => {
           return (
-            <div class="profilepost-div" tabindex="0" key={post._id}>
+            <div className="profilepost-div" key={post._id}>
               <Link to={`/post/${post._id}`}>
-                <img src={post.photo} class="profilepost-img" alt="" />
-                <div class="profilepost-info">
+                <img src={post.photo} className="profilepost-img" alt="" />
+                <div className="profilepost-info">
                   <ul>
-                    <li class="profilepost-likes">
-                      <i class="fas fa-heart" aria-hidden="true"></i> {post.likesCount}
+                    <li className="profilepost-likes">
+                      <i className="fas fa-heart" aria-hidden="true"></i> {post.likesCount}
                     </li>
-                    <li class="profilepost-comments">
-                      <i class="fas fa-comment" aria-hidden="true"></i> {post.commentsCount}
+                    <li className="profilepost-comments">
+                      <i className="fas fa-comment" aria-hidden="true"></i> {post.commentsCount}
                     </li>
                   </ul>
                 </div>

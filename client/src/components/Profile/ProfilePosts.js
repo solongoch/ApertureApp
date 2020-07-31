827461e426c506718addffa758460b5b4a6a7234
Lakshmipriya <slpriya.as@gmail.com>
2020-07-31 03:56:28 -0700

    ProfilePosts UI Allignment issue fixed

--- client/src/components/Profile/ProfilePosts.js
+++ client/src/components/Profile/ProfilePosts.js
@@ -1,44 +1,37 @@
-import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './profileposts.css';
 
 class ProfilePosts extends Component {
-  constructor() {
-    super();
-    this.state = {
-
-    }
-  }
-
   render() {
+
     const { posts } = this.props;
     return (
-      <div className="row">
+
+      <div class="profileposts">
         {posts.map(post => {
           return (
-            <div className="single-row col-md-4 col-lg-4 col-sm-4 col-12 text-center " 
-            key={post._id}>
-              <div className="post profileposts_div">
-                <img className="image" src={post.photo} alt="Post" />
-                <div className="overlay">
-                  <Link to={`/post/${post._id}`}>
-                    <div className="numbers">
-                      <span className="right-15">
-                        <i className="fa fa-heart"></i> {post.likesCount}
-                      </span>
-                      <span>
-                        <i className="fa fa-comment"></i>{post.commentsCount}
-                      </span>
-                    </div>
-                  </Link>
+            <div class="profilepost-div" tabindex="0" key={post._id}>
+              <Link to={`/post/${post._id}`}>
+                <img src={post.photo} class="profilepost-img" alt="" />
+                <div class="profilepost-info">
+                  <ul>
+                    <li class="profilepost-likes">
+                      <i class="fas fa-heart" aria-hidden="true"></i> {post.likesCount}
+                    </li>
+                    <li class="profilepost-comments">
+                      <i class="fas fa-comment" aria-hidden="true"></i> {post.commentsCount}
+                    </li>
+                  </ul>
                 </div>
-              </div>
+              </Link>
             </div>
           )
         })}
       </div>
-    );
+
+    )
   }
 }
-
 
 export default ProfilePosts;

a86abb88d9f5f55508d3c41f505a727383b0953a
Lakshmipriya <slpriya.as@gmail.com>
2020-08-01 13:01:57 -0700

    CHanges as master

--- client/src/components/Post/SinglePost.js
+++ client/src/components/Post/SinglePost.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getPost } from '../../actions/postActions';
 // import CSS
 import './single-post.css';
 // import images
+import post from '../../image/img-sq.jpg';
 import PostSidebar from './PostSidebar';
 
 import PostHeader from './PostHeader';
 
-
 class SinglePost extends Component {
-
-  componentDidMount() {
-    if (this.props.match.params.postId) {
-      let postId = this.props.match.params.postId;
-      this.props.getPost(postId);
-    }
-  }
-
   render() {
-    const { post } = this.props;
     return (
       <div id="single-post-div">
         <div className="single-post">
-          <PostHeader post={post}></PostHeader>
+        <PostHeader></PostHeader>
           <div className="post-image">
-            <img src={post.photo} alt="Post" />
+            <img src={post} alt="Post" />
           </div>
         </div>
-        <PostSidebar post={post}></PostSidebar>
+        <PostSidebar></PostSidebar>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => ({
-  post: state.post.post
-})
-
-export default connect(mapStateToProps, { getPost })(SinglePost);
+export default SinglePost;

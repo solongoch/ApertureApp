9e188f564e1b2901e15b3cde942d32ce069fd632
Lakshmipriya <slpriya.as@gmail.com>
2020-08-03 13:49:18 -0700

    Testing ProfiePage

--- client/src/components/Profile/ProfilePage.js
+++ client/src/components/Profile/ProfilePage.js
@@ -13,38 +13,39 @@ import './profile.css';
 class ProfilePage extends Component {
 
   componentDidMount() {
-    this.props.getProfileByUsername(this.props.match.params.username, this.props.history);
+    var currentUsername = this.props.auth.user.username;
+    this.props.getProfileByUsername(this.props.match.params.username, currentUsername, this.props.history);
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.profileState.profile)
-    {
-      this.setState({posts: nextProps.profileState.profile.posts })
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profileState.profile) {
+      this.setState({ posts: nextProps.profileState.profile.posts })
     }
   }
 
 
   render() {
     const { profile } = this.props.profileState;
+
     if (!profile) {
       return (<Spinner />)
     }
-    if(profile){
+    if (profile) {
       var _posts = profile.posts;
       var _profilePosts;
       if (_posts && _posts.length > 0) { //has post display ProfilePosts component
         _profilePosts = <ProfilePosts posts={_posts} />
       } else { // else display no posts with camera icon 
         _profilePosts = (<div>
-          <h1 className="text-center"><i className="fa fa-camera" style ={{fontSize: "40px"}} /></h1>
+          <h1 className="text-center"><i className="fa fa-camera" style={{ fontSize: "40px" }} /></h1>
           <h3 className="text-center"> No Posts Yet</h3>
         </div>)
       }
     }
     return (
       <div className="profile">
-        <ProfileHeader profile={ profile } auth={ this.props.auth } />
-        <div className="top-post-menu d-flex flex-row justify-content-center"></div>  
+        <ProfileHeader profile={profile} auth={this.props.auth} />
+        <div className="top-post-menu d-flex flex-row justify-content-center"></div>
         {_profilePosts}
       </div>
     )
@@ -53,6 +54,7 @@ class ProfilePage extends Component {
 
 const mapStateToProps = (state) => ({
   profileState: state.profile,
+  searchedProfileState: state.profile,
   auth: state.auth
 });
 

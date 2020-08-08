e13970b5168c6f3e679762581ef3558cddc9800f
Lakshmipriya <slpriya.as@gmail.com>
2020-08-08 13:03:04 -0700

    changes follows output as per the data adding in followingLists from api

--- routes/api/follows.js
+++ routes/api/follows.js
@@ -33,15 +33,13 @@ router.put('/:userId/follow', passport.authenticate('jwt', { session: false }), 
                 myUser.save().then(() => {
 
                   //removed password from return response
-                  user= user.toObject();
-                  delete user.password                    
-                  delete user.__v;
-                 
-                  return res.json({
-                    success: true,
-                    message: `Followed ${userId}`,
-                    user: user
-                })});
+                  user = {
+                    _id: user._id,
+                    name: user.name,
+                    username: user.username,
+                    avatar:user.avatar
+                  }                 
+                  return res.json({ _id :user._id ,user })});
               });
           })
           .catch(err => res.status(500).json({ message: err.msg }));

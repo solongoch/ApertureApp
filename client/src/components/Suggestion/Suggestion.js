67f9209b282a0c669b32950942f640323ac21355
Lakshmipriya <slpriya.as@gmail.com>
2020-08-08 19:53:58 -0700

    Update Suggestion.js
    masterversion

--- client/src/components/Suggestion/Suggestion.js
+++ client/src/components/Suggestion/Suggestion.js
@@ -42,9 +42,9 @@ class Suggestion extends Component {
                   <div className="make-gray left-15">Followed by {suggestion.followers.length} people</div>
                 </div>
               </div>
-               {/* Follow component takes suggestion object as props */}
+               {/* Follow Button */}
               <Follow 
-                user={suggestion}
+                userId={suggestion._id}
               />
             </div>
           ))}
